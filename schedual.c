9e20f930006475184486da8231d2c6af6c20b35c
prasad khairnar <123644702+PrasadKhairnar2003@users.noreply.github.com>
2024-05-05 17:02:00 +0530

    Responsive Web page

---
+++ schedual.c
@@ -0,0 +1,16 @@
+#include<stdio.in>
+
+int main()
+{
+    int arrival_time[10],burst_time[10],temp[10];
+    int 1, smallest, count=0, time, limit;
+    double wait_time=0, turnaround_time=0, end;
+    float average_waiting_time, average_turnaround_time;
+    printf("\nEnter the Total no of processes:\t");
+    scanf("%d",&limit);
+    printf("\nEnter Details of %d Processes\n",limit);
+    for(i=0;i<limit;i++)
+    {
+        
+    }
+}
